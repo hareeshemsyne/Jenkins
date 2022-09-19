3f03c3d803ee1c9328b4f0bf6af22bf70c76254b
hareeshemsyne <hareesh1@emsyne.com>
2022-09-19 11:26:09 +0530

    firstjenkin

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JenkinsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
